338abd8ae3bcbc4c34250927db5100d0be3778e0
Yash Karandikar <nerdstep710@gmail.com>
2024-08-06 13:18:54 -0500

    Add support for reading/writing id3 tags

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use id3::Tag as Id3InternalTag;
+use std::path::Path;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Given file does not have a file extension")]
+    NoFileExtension,
+    #[error("File extension must be valid unicode")]
+    InvalidFileExtension,
+    #[error("Unsupported file format")]
+    UnsupportedFormat,
+    #[error("{0}")]
+    Id3Error(#[from] id3::Error),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type Result<T> = std::result::Result<T, Error>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub enum Tag {
+    Id3Tag { inner: Id3InternalTag },
+}
+
+impl Tag {
+    pub fn read_from_path<P: AsRef<Path>>(path: P) -> Result<Tag> {
+        let path = path.as_ref();
+        let extension = path
+            .extension()
+            .ok_or(Error::NoFileExtension)?
+            .to_str()
+            .ok_or(Error::InvalidFileExtension)?;
+        match extension {
+            "mp3" | "wav" | "aiff" => {
+                let inner = Id3InternalTag::read_from_path(path)?;
+                Ok(Tag::Id3Tag { inner })
+            }
+            _ => Err(Error::UnsupportedFormat),
+        }
+    }
+
+    pub fn write_to_path<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        match self {
+            Self::Id3Tag { inner } => inner.write_to_path(path, id3::Version::Id3v24)?,
+        };
+        Ok(())
     }
 }
