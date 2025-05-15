4c8a4dfe6f2ba83076f7515e3c1608674bd97b02
Yash Karandikar <nerdstep710@gmail.com>
2025-05-14 19:14:29 -0500

    Add an example for reading tags

---
+++ examples/read_tag.rs
@@ -0,0 +1,25 @@
+use multitag::Tag;
+use std::env::args;
+use std::fs::File;
+use std::io::Cursor;
+use std::io::Read;
+use std::path::PathBuf;
+
+fn main() {
+    let path = PathBuf::from(args().nth(1).unwrap());
+
+    // Option 1: read from path
+    let tag = Tag::read_from_path(&path).unwrap();
+    println!("{:#?}", tag.title());
+
+    // Option 2: read from reader
+    let mut f = File::open(&path).unwrap();
+
+    let mut data = Vec::new();
+    f.read_to_end(&mut data).unwrap();
+
+    let cursor = Cursor::new(data);
+    // You can also just pass in f instead of creating a cursor since Files are Read + Seek
+    let tag = Tag::read_from(path, cursor).unwrap();
+    println!("{:#?}", tag.title());
+}
